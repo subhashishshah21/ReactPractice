d23108740bbefedd187c8a2f82bd21e4e1f1051e
subhashishshah21 <subhashishshahprofessional@gmail.com>
2021-07-29 08:37:01 +0545

    menu practice

---
+++ menuReact/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ['all',...new Set(items.map((item)=>item.category))
+] 
+function App() {
+  const [menuItems,setmenuItems] = useState(items);
+  const [categories,setcategories]= useState(allCategories);
+  
+  const filterItems = (category)=>{
+    const newItems = items.filter((item)=>
+      item.category === category
+    )
+    setmenuItems(newItems);
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menus</h2>  
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items = {menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
